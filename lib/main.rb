e7ed79c038173d16c08668161ee3c23de75834a0
akihiro <yamamori.a.aa@m.titech.ac.jp>
2014-06-07 10:59:22 +0900

    Refactor: Remane in some variables

--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,12 @@
 #Find the number p where p^2 - 1 has 300 divisors.
 
-for i in 1..1000000000 do
-  prime = i ** 2 -1
+for p in 1..1000000000 do
+  num = p ** 2 -1
   count = 0
-  for j in 1..Math.sqrt(prime)do
-    count+=1 if prime%j ==0
+  for div in 1..Math.sqrt(num)do
+    count+=1 if num%div ==0
   end
   break if count==300/2
 end
-puts "p=" + i
-puts "p^2-1=" + i ** 2 -1
+puts "p=" + p.to_s
+puts "p^2-1=" + (p ** 2 -1).to_s
